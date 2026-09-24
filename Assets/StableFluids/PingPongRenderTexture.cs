60b9450db68a8b5529a63310420f10db180e2b52
haharman <haru056489@gmail.com>
2026-09-24 17:42:19 +0900

    feat: implement StableFluids
    feat: implement GPU-based Stable Fluids with compute shaders
    
    - コンピュートシェーダーで Stable Fluids（外力・移流・投影）を実装
      - Semi-Lagrangian 移流で、dt が大きくても破綻しない移流を実現
      - 発散 → 圧力 Jacobi 反復 → 勾配の減算による投影で非圧縮性を近似し、渦を再現
      - 周期境界を、サンプラー（Repeat）と添字の剰余計算で統一
    - PingPongRenderTexture を実装
      - 読み書きの競合を避け、各ステップを「Bind → Dispatch → Swap」の形に統一
      - IDisposable で RenderTexture の解放を保証
    - ポインター入力による速度注入を実装
      - 入力を UV 空間で抽象化し、解像度や入力デバイスに依存しない構成に
      - ガウス減衰でなめらかに速度を加算
    - スレッドグループ数を切り上げで算出し、任意の解像度に対応

--- Assets/StableFluids/PingPongRenderTexture.cs
+++ Assets/StableFluids/PingPongRenderTexture.cs
@@ -1,8 +1,9 @@
+using System;
 using UnityEngine;
 
 namespace StableFluids
 {
-    public class PingPongRenderTexture
+    public class PingPongRenderTexture : IDisposable
     {
         private RenderTexture _read;
         private RenderTexture _write;
@@ -12,9 +13,9 @@ namespace StableFluids
 
         public PingPongRenderTexture(RenderTexture firstRead, RenderTexture firstWrite)
         {
-            if(firstRead == null) throw new System.ArgumentNullException(nameof(firstRead));
-            if(firstWrite == null) throw new System.ArgumentNullException(nameof(firstWrite));
-            if(firstRead == firstWrite) throw new System.ArgumentException("firstRead と firstWrite が同じ参照です");
+            if(firstRead == null) throw new ArgumentNullException(nameof(firstRead));
+            if(firstWrite == null) throw new ArgumentNullException(nameof(firstWrite));
+            if(firstRead == firstWrite) throw new ArgumentException("firstRead と firstWrite が同じ参照です");
             _read = firstRead;
             _write = firstWrite;
         }
@@ -22,6 +23,23 @@ namespace StableFluids
         public void Swap()
         {
             (_read, _write) = (_write, _read);
+        }
+
+        public void ClearRead()
+        {
+            var current = RenderTexture.active;
+            RenderTexture.active = _read;
+            GL.Clear(false, true, Color.clear);
+            RenderTexture.active = current;
+        }
+        
+        public void Dispose()
+        {
+            if (_read != null) { UnityEngine.Object.Destroy(_read); }
+            if (_write != null) { UnityEngine.Object.Destroy(_write); }
+            
+            _read = null;
+            _write = null;
         }
     }
 }
--- Assets/StableFluids/StableFluids.cs
+++ Assets/StableFluids/StableFluids.cs
@@ -9,20 +9,32 @@ namespace StableFluids
         [SerializeField] private ComputeShader shader;
         [SerializeField] private int w;
         [SerializeField] private int h;
+        [SerializeField, Range(1, 1000)] private int pressureJacobiCount;
         [SerializeField] private Texture2D dyeInitTex;
         [SerializeField] private RawImage rawImage;
 
-        private PingPongRenderTexture _dyePingPong;
-        private PingPongRenderTexture _velPingPong;
-        
-        private int _kernelId;
-        private Vector3Int _groupSize;
+        private PingPongRenderTexture _dye;
+        private PingPongRenderTexture _velocity;
+        private PingPongRenderTexture _pressure;
+        private RenderTexture _divergenceRt;
         
         private int _addVelocityKernelIndex;
         private Vector3Int _addVelocityGroupSize;
 
+        private int _advectVelocityKernelIndex;
+        private Vector3Int _advectVelocityGroupSize;
+        
+        private int _divergenceKernelIndex;
+        private Vector3Int _divergenceGroupSize;
+        
+        private int _pressureJacobiKernelIndex;
+        private Vector3Int _pressureJacobiGroupSize;
+
         private int _advectDyeKernelIndex;
         private Vector3Int _advectDyeGroupSize;
+
+        private int _subtractPressureGradientKernelIndex;
+        private Vector3Int _subtractPressureGradientGroupSize;
         
         private void Start()
         {
@@ -31,44 +43,67 @@ namespace StableFluids
            var dyeFirstWriteRt = new RenderTexture(w, h, 0, RenderTextureFormat.ARGBFloat) { enableRandomWrite = true };
            var velFirstReadRt = new RenderTexture(w, h, 0, RenderTextureFormat.RGFloat) { enableRandomWrite = true };
            var velFirstWriteRt = new RenderTexture(w, h, 0, RenderTextureFormat.RGFloat) { enableRandomWrite = true };
+           var presFirstReadRt = new RenderTexture(w, h, 0, RenderTextureFormat.RFloat) { enableRandomWrite = true };
+           var presFirstWriteRt = new RenderTexture(w, h, 0, RenderTextureFormat.RFloat) { enableRandomWrite = true };
+           _divergenceRt = new  RenderTexture(w, h, 0, RenderTextureFormat.RFloat) { enableRandomWrite = true };
            
            // RenderTexture のVRAM確保
            dyeFirstReadRt.Create();
            dyeFirstWriteRt.Create();
            velFirstReadRt.Create();
            velFirstWriteRt.Create();
+           presFirstReadRt.Create();
+           presFirstWriteRt.Create();
+           _divergenceRt.Create();
            
            // 初期状態を書き込む
            Graphics.Blit(Texture2D.blackTexture, velFirstReadRt);
            Graphics.Blit(dyeInitTex, dyeFirstReadRt);
            
            // PingPongラッパーにする
-           _dyePingPong = new PingPongRenderTexture(dyeFirstReadRt, dyeFirstWriteRt);
-           _velPingPong = new PingPongRenderTexture(velFirstReadRt, velFirstWriteRt);
+           _dye = new PingPongRenderTexture(dyeFirstReadRt, dyeFirstWriteRt);
+           _velocity = new PingPongRenderTexture(velFirstReadRt, velFirstWriteRt);
+           _pressure = new PingPongRenderTexture(presFirstReadRt, presFirstWriteRt);
            
-           // KernelIndex, GroupSize(Threadsの数)を取得
+           // KernelIndexを取得
            _addVelocityKernelIndex = shader.FindKernel("AddVelocity");
+           _advectVelocityKernelIndex = shader.FindKernel("AdvectVelocity");
+           _divergenceKernelIndex = shader.FindKernel("Divergence");
+           _pressureJacobiKernelIndex  = shader.FindKernel("PressureJacobi");
+           _subtractPressureGradientKernelIndex = shader.FindKernel("SubtractPressureGradient");
            _advectDyeKernelIndex = shader.FindKernel("AdvectDye");
-           _kernelId = shader.FindKernel("KernelA");
+           
+           // GroupSize(Threads数)を取得
            uint x, y, z;
-           shader.GetKernelThreadGroupSizes(_kernelId, out x, out y, out z);
-           _groupSize = new Vector3Int((int)x, (int)y, (int)z);
            shader.GetKernelThreadGroupSizes(_addVelocityKernelIndex, out x, out y, out z);
            _addVelocityGroupSize = new Vector3Int((int)x, (int)y, (int)z);
+           shader.GetKernelThreadGroupSizes(_advectVelocityKernelIndex, out x, out y, out z);
+           _advectVelocityGroupSize = new Vector3Int((int)x, (int)y, (int)z);
+           shader.GetKernelThreadGroupSizes(_divergenceKernelIndex, out x, out y, out z);
+           _divergenceGroupSize = new Vector3Int((int)x, (int)y, (int)z);
+           shader.GetKernelThreadGroupSizes(_pressureJacobiKernelIndex, out x, out y, out z);
+           _pressureJacobiGroupSize = new Vector3Int((int)x, (int)y, (int)z);
+           shader.GetKernelThreadGroupSizes(_subtractPressureGradientKernelIndex, out x, out y, out z);
+           _subtractPressureGradientGroupSize = new Vector3Int((int)x, (int)y, (int)z);
            shader.GetKernelThreadGroupSizes(_advectDyeKernelIndex, out x, out y, out z);
            _advectDyeGroupSize = new Vector3Int((int)x, (int)y, (int)z);
            
            // 反映テスト
-           rawImage.texture = _dyePingPong.read;
+           rawImage.texture = _dye.read;
 
            //shader.SetTexture(_kernelId, "_Buffer", _rt);
         }
 
         private void Update()
         {
+            // 共通の値をセット
+            shader.SetFloat("_DeltaTime", Time.deltaTime);
+            shader.SetInts("_Resolution", w, h);
+            
+            #region Add Velocity
             if(velocityInputMock.TryGetVelocityInput(out VelocityInputData velocityInput))
             {
-                // 速度入力データをスクリーン座標に変換
+                // 速度入力データを格子座標に変換
                 var position = new Vector2(velocityInput.PositionUv.x * w, velocityInput.PositionUv.y * h);
                 var velocity = new Vector2(velocityInput.VelocityUv.x * w, velocityInput.VelocityUv.y * h);
                 var radius = velocityInput.Radius * h;
@@ -77,30 +112,98 @@ namespace StableFluids
                 shader.SetVector("_InputVelocity", velocity);
                 shader.SetFloat("_InputRadius", radius);
                 
-                shader.SetTexture(_addVelocityKernelIndex, "_VelocityBufferRead", _velPingPong.read);
-                shader.SetTexture(_addVelocityKernelIndex, "_VelocityBufferWrite", _velPingPong.write);
+                shader.SetTexture(_addVelocityKernelIndex, "_VelocityBufferRead", _velocity.read);
+                shader.SetTexture(_addVelocityKernelIndex, "_VelocityBufferWrite", _velocity.write);
+                
                 shader.Dispatch(_addVelocityKernelIndex,
-                    w / _addVelocityGroupSize.x,
-                    h / _addVelocityGroupSize.y,
-                    _addVelocityGroupSize.z);
+                    (w + _addVelocityGroupSize.x - 1) / _addVelocityGroupSize.x,
+                    (h + _addVelocityGroupSize.y - 1) / _addVelocityGroupSize.y,
+                    1);
                 
                 // Read用RTとWrite用RTを内部で入れ替える
-                _velPingPong.Swap();
+                _velocity.Swap();
             }
             
-            shader.SetFloat("_DeltaTime", Time.deltaTime);
-            shader.SetInts("_Resolution", w, h);
+            #endregion
+            #region Advect Velocity
             
-            shader.SetTexture(_advectDyeKernelIndex, "_VelocityBufferRead", _velPingPong.read);
-            shader.SetTexture(_advectDyeKernelIndex, "_DyeBufferRead", _dyePingPong.read);
-            shader.SetTexture(_advectDyeKernelIndex, "_DyeBufferWrite", _dyePingPong.write);
+            shader.SetTexture(_advectVelocityKernelIndex, "_VelocityBufferRead", _velocity.read);
+            shader.SetTexture(_advectVelocityKernelIndex, "_VelocityBufferWrite", _velocity.write);
+            
+            shader.Dispatch(_advectVelocityKernelIndex,
+                (w + _advectVelocityGroupSize.x - 1) / _advectVelocityGroupSize.x,
+                (h + _advectVelocityGroupSize.y - 1) / _advectVelocityGroupSize.y,
+                1);
+            
+            _velocity.Swap();
+            #endregion
+            #region Divergence
+            
+            shader.SetTexture(_divergenceKernelIndex, "_VelocityBufferRead", _velocity.read);
+            shader.SetTexture(_divergenceKernelIndex, "_DivergenceBufferWrite", _divergenceRt);
+            
+            shader.Dispatch(_divergenceKernelIndex,
+                (w + _divergenceGroupSize.x - 1) / _divergenceGroupSize.x,
+                (h + _divergenceGroupSize.y - 1) / _divergenceGroupSize.y,
+                1);
+            
+            #endregion
+            #region PressureJacobi
+            
+            _pressure.ClearRead();
+            
+            shader.SetTexture(_pressureJacobiKernelIndex, "_DivergenceBufferRead", _divergenceRt);
+            
+            for (int i = 0; i < pressureJacobiCount; i++)
+            {
+                shader.SetTexture(_pressureJacobiKernelIndex, "_PressureBufferRead", _pressure.read);
+                shader.SetTexture(_pressureJacobiKernelIndex, "_PressureBufferWrite", _pressure.write);
+                shader.Dispatch(_pressureJacobiKernelIndex,
+                    (w + _pressureJacobiGroupSize.x - 1) / _pressureJacobiGroupSize.x,
+                    (h + _pressureJacobiGroupSize.y - 1) / _pressureJacobiGroupSize.y,
+                    1);
+                _pressure.Swap();
+            }
+            
+            #endregion
+            #region PressureJacobi
+            
+            shader.SetTexture(_subtractPressureGradientKernelIndex, "_PressureBufferRead", _pressure.read);
+            shader.SetTexture(_subtractPressureGradientKernelIndex,  "_VelocityBufferRead", _velocity.read);
+            shader.SetTexture(_subtractPressureGradientKernelIndex, "_VelocityBufferWrite", _velocity.write);
+            shader.Dispatch(_subtractPressureGradientKernelIndex,
+                (w + _subtractPressureGradientGroupSize.x - 1) / _subtractPressureGradientGroupSize.x,
+                (h + _subtractPressureGradientGroupSize.y - 1) / _subtractPressureGradientGroupSize.y,
+                1);
+            _velocity.Swap();
+            
+            #endregion
+            #region Advect Dye
+            
+            shader.SetTexture(_advectDyeKernelIndex, "_VelocityBufferRead", _velocity.read);
+            shader.SetTexture(_advectDyeKernelIndex, "_DyeBufferRead", _dye.read);
+            shader.SetTexture(_advectDyeKernelIndex, "_DyeBufferWrite", _dye.write);
+            
             shader.Dispatch(_advectDyeKernelIndex,
-                w / _advectDyeGroupSize.x,
-                h / _advectDyeGroupSize.y,
-                _advectDyeGroupSize.z);
+                (w + _advectDyeGroupSize.x - 1) / _advectDyeGroupSize.x,
+                (h + _advectDyeGroupSize.y - 1) / _advectDyeGroupSize.y,
+                1);
             
-            _dyePingPong.Swap();
+            _dye.Swap();
             
+            #endregion
+            
+            //rawImage.texture = _dye.read;
+            rawImage.texture = _dye.read;
+        }
+
+        private void OnDestroy()
+        {
+            _velocity?.Dispose();
+            if(_divergenceRt != null) { UnityEngine.GameObject.Destroy(_divergenceRt); }
+            _divergenceRt = null;
+            _pressure?.Dispose();
+            _dye?.Dispose();
         }
     }
 }
